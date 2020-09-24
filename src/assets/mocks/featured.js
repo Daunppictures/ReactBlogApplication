0db1793cdf21abd9d89321279ac1aaa0fa1151fc
Daunpictures <solodovnikov.toni@gmail.com>
2020-09-24 19:44:25 +0300

    1.2 (Added posts)

---
+++ src/assets/mocks/featured.js
@@ -0,0 +1,32 @@
+import moment from 'moment';
+
+export default [
+    {
+        title: 'Can anyone code?',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture', 'Tech News'],
+        link: '#',
+        image: 'anyone_can_code.png'
+    },
+    {
+        title: 'Using AWS S3 for Storing Blog Images',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Cloud'],
+        link: '#',
+        image: 'cloud.jpeg'
+    },
+    {
+        title: 'Popular Programming Languages in 2020',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech News', 'Tech Culture'],
+        link: '#',
+        image: 'languages.jpeg'
+    },
+    {
+        title: 'Brain Hacks for Learning to Program',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Brain Health'],
+        link: '#',
+        image: 'hacks.jpg'
+    }
+]
---
+++ src/assets/mocks/trending.js
@@ -0,0 +1,39 @@
+import moment from 'moment';
+
+export default [
+    {
+        title: 'Software Engineer Salary in 2020',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture'],
+        link: '#',
+        image: 'software_engineer.jpg'
+    },
+    {
+        title: 'GraphQL vs REST',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['React', 'JavaScript'],
+        link: '#',
+        image: 'graphql_vs_rest.png'
+    },
+    {
+        title: 'A Day in the Life of a Programmer',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture'],
+        link: '#',
+        image: 'programmer_day.jpg'
+    },
+    {
+        title: 'Brain Hacks for Learning to Program',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Brain Health'],
+        link: '#',
+        image: 'hacks.jpg'
+    },
+    {
+        title: 'React vs Vue',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['React', 'Vue'],
+        link: '#',
+        image: 'react_vs_vue.jpg'
+    }
+]
